576624c4c8f5b1c179970d4dbe892aea33e4cd47
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-15 18:56:05 +0530

    [Ankush] Add:Check the max of 3 number using test

---
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/MaxNumberCheck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    public class MaxNumberCheck
+    {
+        //check Foat Maximum int Number
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        //comapring First Number to Second Number > 0  and firstNumber to third Number >0  
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                return firstValue;
+            //comapring Second Number to First Number  > 0  and SecondNumber to third Number >0  
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                //return secondNumber
+                return secondValue;
+            else
+                //return thirdNumber
+                return thirdValue;
+
+        }
+        //check Foat Maximum Float Number
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            //comapring First Number to Second Number > 0  and firstNumber to third Number >0  
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                //return 
+                return firstValue;
+            //comapring Second Number to First Number  > 0  and SecondNumber to third Number >0  
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                //return secondNumber
+                return secondValue;
+            else
+                //return thirdNumber
+                return thirdValue;
+        }
+
+    }
+}
